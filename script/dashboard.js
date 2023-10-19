42183871d9058cec48faa7ba3e3e66b5cf266a0a
alfian <devsroot8@gmail.com>
2023-10-19 14:50:28 +0700

    fix: change idr nominal for product price

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,6 +1,14 @@
 const URL = "https://6526b95b917d673fd76ce548.mockapi.io/products";
 const listProduct = document.getElementById("product-container");
 
+function formatCurrency(number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+}
+
 async function getAllProducts() {
   const response = await fetch(
     "https://6526b95b917d673fd76ce548.mockapi.io/products"
@@ -20,7 +28,9 @@ async function getAllProducts() {
             <div class="p-2">
                 <h1 class="text-md font-bold">${item.name}</h1>
                 <span class="text-xs text-slate-500 mb-2">${item.from}</span>
-                <p class="font-md font-semibold mb-2">${item.price}</p>
+                <p class="font-md font-semibold mb-2">${formatCurrency(
+                  item.price
+                )}</p>
                 <div class="flex items-center gap-2 mb-4">
                 <div class="border-r-2 pr-2">
                     <i class="fa-solid fa-star text-yellow-500"></i>
@@ -29,6 +39,7 @@ async function getAllProducts() {
                 <span>50 terjual</span>
                 </div>
                 <button
+                id="cart-btn"
                 type="button"
                 class="bg-red-700 p-2 flex justify-end rounded-md"
                 >
